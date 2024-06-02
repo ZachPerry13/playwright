cda88c3b7d9a052aa372309b2281d4e632ee7c7d
Zach Perry <72410061+ZachPerry13@users.noreply.github.com>
2024-06-02 17:06:41 -0400

    Cleaned up/ made into two tests

--- zachperryonline.spec.ts
+++ zachperryonline.spec.ts
@@ -17,15 +17,6 @@ async function emptyDirectory(directory) {
 // Call the function to empty the directory before running the tests
 emptyDirectory('./screenshots'); // Provide the directory path you want to empty
 
-
-setup('authenticate', async ({ page }) => {
-
-await page.goto('https://zachperry.online/register');
-
-await page.screenshot({ path: 'screenshots/1-register.png' });
-console.log('zachperry.online')
-
-//Variables for account creation
 // Function to generate random string
 function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -36,48 +27,49 @@ function generateRandomString(length) {
   return result;
 }
 
-// Generate random email
+// Generate random email, username and password 
 const desired_email = generateRandomString(8) + "@gmail.com";
-
-// Generate random username
 const desired_username = "tester" + Math.floor(Math.random() * 1000);
-
-// Generate random password
 const desired_password = generateRandomString(8) + "@" + Math.floor(Math.random() * 10000) + "!";
 
-// Generate random Firstname
-const desired_firstname = "tester" + Math.floor(Math.random() * 1000);
 
-console.log("Randomly generated email:", desired_email);
-console.log("Randomly generated username:", desired_username);
-console.log("Randomly generated password:", desired_password);
-console.log("Randomly generated Firstname:", desired_firstname);
 
-const all = await page.getByRole('textbox').all();
-console.log(all)
+setup('Register', async ({ page }) => {
 
+//Navigate to Website
+await page.goto('https://zachperry.online/register');
+await page.screenshot({ path: 'screenshots/1-register.png' });
+
+//Registed a new account
 await page.getByRole('textbox').first().fill(desired_username);
 await page.getByRole('textbox').nth(1).fill(desired_email);
 await page.getByRole('textbox').nth(2).fill(desired_password);
 await page.screenshot({ path: 'screenshots/2-filledin_registration.png' });
 
-
-/* const all2 = await page.getByRole('button').all();
-console.log(all2) */
-
-//Debug to throw error and get all button id's
-await page.getByRole('button').click();
-console.log('Clicked');
+await page.getByRole('button').first().click();
+console.log('Registered');
 
 console.log("New account created")
 console.log("Email: " + desired_email)
 console.log("Username: " + desired_username)
 console.log("Pasword: " + desired_password)
-
 await page.screenshot({ path: 'screenshots/3-registered.png' });
 
-console.log("Attempting to log into the newly created account");
 
+})
+
+
+
+
+setup('Login', async ({ page }) => {
+
+//Navigate to Website
+await page.goto('https://zachperry.online/login');
+await page.screenshot({ path: 'screenshots/4-login.png' });
+ 
+//Log Into the new Account
+console.log("Attempting to log into the newly created account");
+await page.screenshot({ path: 'screenshots/debug.png' });
 
 await page.getByRole('textbox').first().fill(desired_email);
 await page.getByRole('textbox').nth(1).fill(desired_password);
@@ -85,9 +77,11 @@ await page.getByRole('textbox').nth(1).fill(desired_password);
 await page.getByRole('button').click();
 console.log('Clicked');
 
-await page.screenshot({ path: 'screenshots/4-Logged_in.png' });
+await page.screenshot({ path: 'screenshots/5-Logged_in.png' });
 console.log("Logged into new Account")
 
+
+//Make Sure you are logged in by accessing the All Videos Page
 const all2 = (await page.getByRole('link').all());
 console.log(all2)
 await page.getByRole('link').first().click();
@@ -96,4 +90,5 @@ await page.screenshot({ path: 'screenshots/5-Clicked_all_videos.png' });
 const pagetitle = await page.title();
 console.log("Page Title: " + pagetitle)
 
-});
+}
+);
